c1f257b53ee5f24eaeb454dd0d414387a462d979
pega_user <s8827021@gmail.com>
2022-06-24 18:17:50 +0800

    Implement Login register

---
+++ app/Models/Entities/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\Entities;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'title', 'content'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Entities\User');
+    }
+
+}
---
+++ database/migrations/2022_06_24_083220_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('title');
+            $table->string('content');
+            $table->timestamps();
+
+            $table->string('XXX_1')->nullable();
+            $table->string('XXX_2')->unique();
+            $table->string('XXX_3')->index();
+            $table->string('XXX_4')->default(1);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
